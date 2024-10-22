6775d083ad2abdb2609cb08981f5ce2dd14715fd
cvisotski <corey.visotski@wattswater.com>
2024-10-22 13:20:00 -0400

    Update initial commit

--- zudoku.config.ts
+++ zudoku.config.ts
@@ -1,6 +1,29 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
+    page: {
+      pageTitle: "Documentations",
+      logo: {
+        src: {
+          light: "/logos/nexa_logo.svg",
+          dark: "/logos/nexa_logo.svg"
+        },
+        "width": "99px"
+      }
+    },
+         // ...
+    theme: {
+     light: {
+          primary: "227 100% 36.3%",
+          primaryForeground: "0 0% 100%"
+      },
+      dark: {
+          primary: "227 100% 36.3%",
+          primaryForeground: "0 0% 100%"
+        }
+      },
+      // ... 
+    // ...
   topNavigation: [
     { id: "docs", label: "Documentation" },
     { id: "api", label: "API Reference" },
@@ -17,7 +40,7 @@ const config: ZudokuConfig = {
   redirects: [{ from: "/", to: "/docs/introduction" }],
   apis: {
     type: "file",
-    input: "./apis/openapi.yaml",
+    input: "./apis/vendor.json",
     navigationId: "api",
   },
   docs: {
